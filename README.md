62ec326a351e9ff17425845714e4a8eb9d643f6e
winterdelta <winterdelta@users.noreply.github.com>
2023-02-02 14:03:37 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# nextjs-kgzt7h
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/nextjs-kgzt7h)
---
+++ pages/index.js
@@ -0,0 +1,15 @@
+import Head from 'next/head';
+import styles from '../styles/Home.module.css';
+// import AudioReactRecorder from 'audio-react-recorder';
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Create Next App</title>
+      </Head>
+
+      <main className={styles.main}>AUDIO COMPONENT</main>
+    </div>
+  );
+}
